92d9cd51ddb3bf86453277a3cc8a39e32f334ca9
Sidhantt29 <sidhantt29@gmail.com>
2024-03-08 16:43:21 +0530

    Library Management System
    Laravel Project

---
+++ routes/web.php
@@ -0,0 +1,65 @@
+<?php
+
+use App\Http\Controllers\auth\LoginController;
+use App\Http\Controllers\AutherController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\BookIssueController;
+use App\Http\Controllers\dashboardController;
+use App\Http\Controllers\StudentController;
+use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+})->middleware('guest');
+Route::post('/', [LoginController::class, 'login'])->name('login');
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+// Route::post('/Change-password', [LoginController::class, 'changePassword'])->name('change_password');
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('change-password',[dashboardController::class,'change_password_view'])->name('change_password_view');
+    Route::post('change-password',[dashboardController::class,'change_password'])->name('change_password');
+    Route::get('/dashboard', [dashboardController::class, 'index'])->name('dashboard');
+
+    // author CRUD
+    Route::get('/authors', [AutherController::class, 'index'])->name('authors');
+    Route::get('/authors/create', [AutherController::class, 'create'])->name('authors.create');
+    Route::get('/authors/edit/{auther}', [AutherController::class, 'edit'])->name('authors.edit');
+    Route::post('/authors/update/{id}', [AutherController::class, 'update'])->name('authors.update');
+    Route::post('/authors/delete/{id}', [AutherController::class, 'destroy'])->name('authors.destroy');
+    Route::post('/authors/create', [AutherController::class, 'store'])->name('authors.store');
+ 
+    // books CRUD
+    Route::get('/books', [BookController::class, 'index'])->name('books');
+    Route::get('/book/create', [BookController::class, 'create'])->name('book.create');
+    Route::get('/book/edit/{book}', [BookController::class, 'edit'])->name('book.edit');
+    Route::post('/book/update/{id}', [BookController::class, 'update'])->name('book.update');
+    Route::post('/book/delete/{id}', [BookController::class, 'destroy'])->name('book.destroy');
+    Route::post('/book/create', [BookController::class, 'store'])->name('book.store');
+
+    // students CRUD
+    Route::get('/students', [StudentController::class, 'index'])->name('students');
+    Route::get('/student/create', [StudentController::class, 'create'])->name('student.create');
+    Route::get('/student/edit/{student}', [StudentController::class, 'edit'])->name('student.edit');
+    Route::post('/student/update/{id}', [StudentController::class, 'update'])->name('student.update');
+    Route::post('/student/delete/{id}', [StudentController::class, 'destroy'])->name('student.destroy');
+    Route::post('/student/create', [StudentController::class, 'store'])->name('student.store');
+    Route::get('/student/show/{id}', [StudentController::class, 'show'])->name('student.show');
+
+
+
+
+
+});
